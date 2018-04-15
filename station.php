5153f5c15a815e922992a05455e5d8e70f21962e
Avinash Yadav <itm2016004@iiita.ac.in>
2018-04-16 00:06:36 +0530

    Add files via upload

---
+++ station.php
@@ -0,0 +1,58 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Acolyte | Citizen</title>
+    <link rel="shortcut icon" type="image/x-icon" href="images/lgo1.gif" />
+    <link href="css/login.css" rel="stylesheet">
+    <link href="css/citizen.css" rel="stylesheet">
+  </head>
+
+  <body>
+    <nav class="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0">
+      <a class="navbar-brand col-sm-3 col-md-2 mr-0" href="#">Acolyte</a>
+ 
+    </nav>
+
+    <div class="container-fluid">
+      <div class="row">
+        <nav class="col-md-2 d-none d-md-block bg-light sidebar">
+          <div class="sidebar-sticky">
+            <ul class="nav flex-column">
+              <li class="nav-item">
+                <a class="nav-link active" href="log1.php">
+                  <span data-feather="map-pin"></span>
+                  Know your Police Station<span class="sr-only">(current)</span>
+                </a>
+              </li>
+              </ul>
+              
+          </div>
+        </nav>
+
+ <main role="main" class="col-md-9 ml-sm-auto col-lg-10 pt-3 px-4">
+          <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pb-2 mb-3 border-bottom">
+            <h1 class="h2">Police Station Details</h1>
+            
+          </div>
+        
+
+    <div class="container">
+      <center>
+        <iframe src="https://www.google.com/maps/embed?pb=!1m16!1m12!1m3!1d3430456.261825633!2d79.22561915960591!3d27.179578771476947!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!2m1!1spolice+station+in+uttar+pradesh!5e0!3m2!1sen!2sin!4v1523166887162" width="600" height="450" frameborder="0" style="border:0" allowfullscreen></iframe>
+     </center>
+    </div>
+
+
+      
+
+
+    <!-- Icons -->
+    <script src="https://unpkg.com/feather-icons/dist/feather.min.js"></script>
+    <script>
+      feather.replace()
+    </script>
+
+  
+  </body>
+</html>
